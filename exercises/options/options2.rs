666c31dfe5bf25f09c6b4b84e4dbdab23c1c2935
Spas Zahov <spaszahov@gmail.com>
2023-07-05 21:44:34 +0300

    solutions to all options and errors2

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,7 +1,6 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
@@ -11,8 +10,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
+        } else { 
+            println!("No match");
         }
     }
 
@@ -29,9 +30,12 @@ mod tests {
 
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+        //In Rust, the .flatten() method is used to simplify nested Option values. It operates on an Option<Option<T>> and returns an Option<T>. 
+        //If the outer Option is Some, it "flattens" it by removing one level of nesting and returning the inner Option. 
+        //If the outer Option is None, it returns None.
+        while let Some(integer) = optional_integers.pop().flatten() {
+                assert_eq!(integer, cursor);
+                cursor -= 1;
         }
 
         assert_eq!(cursor, 0);
